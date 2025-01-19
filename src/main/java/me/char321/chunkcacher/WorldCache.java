e94a4b21e64739cc4f1e273d4eb1befdb2162642
tildejustin <spacepepper3.14@gmail.com>
2025-01-19 13:43:11 -0500

    port to 20w14infinite

--- src/main/java/me/char321/chunkcacher/WorldCache.java
+++ src/main/java/me/char321/chunkcacher/WorldCache.java
@@ -5,21 +5,20 @@ import me.voidxwalker.autoreset.Atum;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.ChunkPos;
-import net.minecraft.util.registry.RegistryKey;
 import net.minecraft.world.ChunkSerializer;
-import net.minecraft.world.World;
 import net.minecraft.world.chunk.Chunk;
+import net.minecraft.world.dimension.DimensionType;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class WorldCache {
-    private static final Map<RegistryKey<World>, Long2ObjectLinkedOpenHashMap<CompoundTag>> cache = new HashMap<>();
+    private static final Map<DimensionType, Long2ObjectLinkedOpenHashMap<CompoundTag>> cache = new HashMap<>();
     public static List<ChunkPos> strongholdCache;
 
     public static void addChunk(ChunkPos chunkPos, Chunk chunk, ServerWorld world) {
-        cache.computeIfAbsent(world.getRegistryKey(), k -> new Long2ObjectLinkedOpenHashMap<>()).put(chunkPos.toLong(), ChunkSerializer.serialize(world, chunk));
+        cache.computeIfAbsent(world.dimension.getType(), k -> new Long2ObjectLinkedOpenHashMap<>()).put(chunkPos.toLong(), ChunkSerializer.serialize(world, chunk));
     }
 
     public static boolean shouldCache() {
@@ -27,7 +26,7 @@ public class WorldCache {
     }
 
     public static CompoundTag getChunkNbt(ChunkPos chunkPos, ServerWorld world) {
-        Long2ObjectLinkedOpenHashMap<CompoundTag> map = cache.get(world.getRegistryKey());
+        Long2ObjectLinkedOpenHashMap<CompoundTag> map = cache.get(world.dimension.getType());
         if (map == null) return null;
         return map.get(chunkPos.toLong());
     }
